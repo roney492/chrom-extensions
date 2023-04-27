1bc93cab3ce792c2bf56fd4067b7d49625cf584d
Roney Dsilva <roney.dsilva@cdmx.in>
2023-04-27 13:19:49 +0530

    sync

--- Jobsoid-Assistant/js/popup.js
+++ Jobsoid-Assistant/js/popup.js
@@ -272,8 +272,8 @@ syncButton.addEventListener('click', async () => {
             QuestionnaireId: 0,
             VideoScreenId: 0,
             CustomEmail: {
-              Subject: "",
-              Body: ""
+              Subject: pipelineStageId === 71337 ? "CodeMax || Job Application" : "",
+              Body: pipelineStageId === 71337 ? "<div><p>Dear&nbsp;&nbsp;{{FirstName}},</p>\n<p><br></p>\n<p>Thank you so much for your application to {{CompanyName}}.</p>\n<p>Unfortunately, we are not able to pass you on to the next round at this time, as we are looking for someone with more technical skills. If you need further details on the same, feel free to contact us.&nbsp;</p>\n<p>You have our best wishes for success in locating the career opportunity you deserve. We will retain your resume in our files to review for future openings for up to six months. In the event of an appropriate available position, we will not hesitate to contact you.</p>\n<p>We wish you the best of luck in your job search.&nbsp;</p>\n<p><br></p>\n<p><strong>Regards,</strong><br></p>\n<p><strong>Team Human Resources</strong></p>\n<p><strong><img src=\"https://lh3.googleusercontent.com/KnrqWlWoXpp-uxk8y5XAe4zOkzuh3mamsc95tOhwcl-_oFPH7HVehjUeqXOF0-ySluh6838KQP1t4trEcYSJjKPgHbSTTC8nxSVr_rqL3pxNbqsPxQJi7Xals9pQG6pzf236jGjr6HVHogXsSNakeNs\" width=\"251\" height=\"83\" class=\"CToWUd\" data-bit=\"iit\" style=\"--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgb(59 130 246 / 0.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ; cursor: pointer; height: auto; margin-left: 0px; margin-top: 0px;\"></strong></p>\n<p><strong>Email:&nbsp;</strong><a href=\"mailto:%20careers@CdMx.in\" target=\"_blank\">careers@CdMx.in</a></p>\n<p><strong>Address:&nbsp;</strong>Office No A - 201, Asian Pinnacle, Fatorda, Goa - India (403602)</p>\n<p><strong>Mobile:</strong>&nbsp;&nbsp;<a href=\"tel: +91 89561 49326\">+91 89561 49326</a>&nbsp;&nbsp;<strong>Website:</strong><a href=\"http://cdmx.in/\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=http://cdmx.in/&amp;source=gmail&amp;ust=1679755038036000&amp;usg=AOvVaw0uzVJjMcATHXF44EovycMS\">&nbsp;</a><a href=\"http://cdmx.in/\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=http://CdMx.in&amp;source=gmail&amp;ust=1679755038036000&amp;usg=AOvVaw35V6OGMHbTp4BwxXDkliia\">CdMx.in</a></p>\n<table><tbody><tr><td><p><a href=\"https://www.facebook.com/cdmx.in\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=https://www.facebook.com/cdmx.in&amp;source=gmail&amp;ust=1679755038036000&amp;usg=AOvVaw0cMhLqqHSkJL72YieItCFP\"><img src=\"https://lh4.googleusercontent.com/o5gJ-t6zkpxVjGvcQYSw_6oRWZEt-jPyrmjkwoed8qf2p-dvJUFesvh-mmekDzORgL8rY9akL30QOh26mChm3WOm9M0HESGRBhjUCls-NqimE41V6Qp9FJCt_kxv-DU9Kf7eipGgN2AfV2xWTvXPdzo\" width=\"16\" height=\"16\" class=\"CToWUd\" data-bit=\"iit\" style=\"--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgb(59 130 246 / 0.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ; cursor: pointer; height: auto; margin-left: 0px; margin-top: 0px;\"></a></p></td><td><p><a href=\"https://www.linkedin.com/company/codemax-pvt-ltd/mycompany/\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=https://www.linkedin.com/company/codemax-pvt-ltd/mycompany/&amp;source=gmail&amp;ust=1679755038037000&amp;usg=AOvVaw3josgFbkpqqk-1xY7o1SjB\"><strong><img src=\"https://lh4.googleusercontent.com/RmjM0wmaE3nOMoHL2bnQov4E6uAzYm9Jcv4UB4Ve1ptNF0pHBVhmyef95HSH70jgwWy8OaEzFNXbma-hr4snmZFBJqZDpW_xQuqBKbVKeD2dANLXMBLaKD0CjZqMTi4eOLCVL1uoWId5QLzPgiQX6cw\" width=\"16\" height=\"16\" class=\"CToWUd\" data-bit=\"iit\" style=\"--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgb(59 130 246 / 0.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ; cursor: pointer; height: auto; margin-left: 0px; margin-top: 0px;\"></strong></a></p></td><td><p><a href=\"https://twitter.com/cdmxin\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=https://twitter.com/cdmxin&amp;source=gmail&amp;ust=1679755038037000&amp;usg=AOvVaw0YkbrmAGDbdL9lRV-7SmSK\"><strong><img src=\"https://lh4.googleusercontent.com/s4d6PDvXqNUFeme_ql4dblvl85ain5E7Jsan5YrHQsXOpy5vLKyFb6DcOPZefYzxQ-C8AicR8sWkDrLcFMqh2pSMJH_wEPatWZFisXxzBw-dmRkLiWAU00Cy9yTtvXzcwFC5f3H0PcfVksGaOnjICGs\" width=\"16\" height=\"16\" class=\"CToWUd\" data-bit=\"iit\" style=\"--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgb(59 130 246 / 0.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ; cursor: pointer; height: auto; margin-left: 0px; margin-top: 0px;\"></strong></a></p></td></tr></tbody></table>\n<p>This e-mail may contain privileged and confidential information which is the property of CodeMax IT Solutions Pvt. Ltd. It is intended only for the use of the individual or entity to which it is addressed. If you are not the intended recipient, you are not authorized to read, retain, copy, print, distribute, or use this message. If you have received this communication in error, please notify the sender and delete all copies of this message. CodeMax IT Solutions Pvt. Ltd. does not accept any liability for virus-infected e-mail.</p></div>" : ""
             },
             CustomSms: {
               Text: ""
@@ -305,8 +305,36 @@ syncButton.addEventListener('click', async () => {
   }
 });
 
+// Function to check if the user is logged in
+async function checkInterviewSiteStatus() {
+  try {
+    const response = await fetch('https://int-mng.cdmx.io/admin', {
+      method: 'GET',
+      redirect: 'manual',
+      cache: 'no-cache'
+    });
+
+    if (response.ok) {
+      // The request was successful and returned a status code of 200
+      return true;
+    } else {
+      // The request failed or returned a non-200 status code
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
 
 syncButton.addEventListener('click', async () => {
+  const isLoggedIn = await checkInterviewSiteStatus();
+
+  if (!isLoggedIn) {
+    // User is not logged in or offline
+    alert('You are not logged into Interview System. Please log in and try again.');
+    return;
+  }
   try {
     // Show the spinner
     spinner.style.display = 'block';
@@ -321,16 +349,19 @@ syncButton.addEventListener('click', async () => {
         // Get the jobsoid_id and jobsoid_jobid for the current item
         const jobsoidId = item.jobsoid_id;
         const jobsoidJobId = item.jobsoid_jobid;
+        const totalCount = data.count;
+        let processedCount = 0;
 
         // Call the API to retrieve the candidate details
         await new Promise(resolve => setTimeout(resolve, 500)); // Add a 500ms delay
         const candidateResponse = await fetch(`https://app.jobsoid.com/api/candidates/${jobsoidId}/detail`);
         const candidateData = await candidateResponse.json();
 
-        // Find jobId with Status "New" or "LogicTest"
-        const jobId = candidateData.Jobs.find(job => job.Status === "New" || job.Status === "LogicTest");
+        // Find jobId with Status "Logic Test"
+        const jobId = candidateData.Jobs.find(job => job.Status === "Logic Test")?.Id;
 
         if (jobId) {
+          console.log(jobId)
           // Call the API to update jobsoid_jobid
           await fetch(`https://int-mng.cdmx.io/api/admin/tests/update_jobsoid_jobid?jobsoid_id=${jobsoidId}`, {
             method: 'POST',
@@ -340,6 +371,8 @@ syncButton.addEventListener('click', async () => {
             }
           });
           console.log(`Updated jobsoid_jobid for jobsoid_id ${jobsoidId}`);
+          processedCount++;
+          syncButton.textContent = `${processedCount}/${totalCount}`;
         }
       }
     }
